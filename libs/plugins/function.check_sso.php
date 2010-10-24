9e8207955a9880d11b572388ca47b837818cbb13
magsilva <magsilva@9ea4b3f1-8734-0410-8b10-af202f86de43>
2010-10-24 02:47:31 +0000

    - Imported SSO plugins implemented at NUMA. They are related to OpenID   features.

--- libs/plugins/function.check_sso.php
+++ libs/plugins/function.check_sso.php
@@ -37,3 +37,42 @@ function smarty_function_check_sso($params, &$smarty)
 
 
 ?>
+<?php
+/*
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+Copyright (C) 2007 Marco Aurélio Graciotto Silva <magsilva@gmail.com>
+*/
+
+
+function smarty_function_check_sso($params, &$smarty)
+{
+   // call the function
+   $url = $params['url'];
+   $identity = $params['identity'];
+   
+   $result = fopen($url, 'r');
+	if ($result === FALSE) {
+		$result = 'images/error.png';	
+	} else {
+		$result = $url . '?openid_name=' . $identity;
+	}
+
+   // assign template variable
+   $smarty->assign('sso_status', $result);
+}
+
+
+?>
